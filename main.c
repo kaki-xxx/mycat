61e88d7bb598f06bea03820e9843256126ba0f90
kaki <suka6411@gmail.com>
2021-05-05 20:29:41 +0900

    feat: 複数ファイルの指定に対応

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -11,20 +12,23 @@ void die(const char* fmt, ...) {
     exit(EXIT_FAILURE);
 }
 
-void do_cat(const char *path) {
-    FILE *fp = fopen(path, "r");
-    if (fp == NULL) {
-        die("Can't open: %s\n", path);
+void do_cat(const char *path[], size_t n) {
+    for (int i = 0; i < n; i++) {
+        FILE *fp = fopen(path[i], "r");
+        if (fp == NULL) {
+            die("Can't open: %s\n", path[i]);
+        }
+        char buf[256];
+        do {
+            size_t ret = fread(buf, sizeof(*buf), ARRAY_SIZE(buf), fp);
+            fwrite(buf, sizeof(*buf), ret, stdout);
+        } while (!feof(fp));
+        fclose(fp);
     }
-    char buf[256];
-    do {
-        size_t ret = fread(buf, sizeof(*buf), ARRAY_SIZE(buf), fp);
-        fwrite(buf, sizeof(*buf), ret, stdout);
-    } while (!feof(fp));
-    fclose(fp);
 }
 
 int main(int argc, char *argv[]) {
-    do_cat(argv[1]);
+    const char **files = (const char**)argv + 1;
+    do_cat(files, argc - 1);
     return 0;
 }
