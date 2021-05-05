5903189dddd98d2d5eba93c279fcfa2921160101
kaki <suka6411@gmail.com>
2021-05-05 17:28:20 +0900

    fix: ファイルが開けなかったときのためのエラー処理を追加

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+void die(const char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    exit(EXIT_FAILURE);
+}
 
 void do_cat(const char *path) {
     FILE *fp = fopen(path, "r");
+    if (fp == NULL) {
+        die("Can't open: %s\n", path);
+    }
     char buf[256];
     do {
         size_t ret = fread(buf, sizeof(*buf), ARRAY_SIZE(buf), fp);
