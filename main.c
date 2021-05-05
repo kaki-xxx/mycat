b2544fc78448a904aa110d7fd9af128f3cf27db5
kaki <suka6411@gmail.com>
2021-05-05 17:20:51 +0900

    feat: シンプルなcatを実装

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-int main(void) {
-    printf("Hello, World");
+void do_cat(const char *path) {
+    FILE *fp = fopen(path, "r");
+    char buf[256];
+    do {
+        size_t ret = fread(buf, sizeof(*buf), ARRAY_SIZE(buf), fp);
+        fwrite(buf, sizeof(*buf), ret, stdout);
+    } while (!feof(fp));
+    fclose(fp);
+}
+
+int main(int argc, char *argv[]) {
+    do_cat(argv[1]);
     return 0;
 }
